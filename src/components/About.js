caf66536ed56e3dd971efa7b737372cf358f72e0
Yerram Abhilash <abhilashabhi2909@gmail.com>
2023-11-30 16:21:37 +0530

    commis

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 const About = () => {
     return ( 
         <p>
-            <h1> Welcome to <b>NYAAY</b>, your go-to destination for legal guidance and information! Our innovative legal chatbot is designed to simplify the complexities of laws and regulations, providing users with instant assistance and accurate information. Whether you're navigating business laws, seeking legal advice, or simply curious about your rights, our chatbot is here to guide you through the legal landscape. Empower yourself with easy access to legal knowledge, and let our chatbot be your virtual legal companion. Trust <b>NYAAY SAHAYAAK</b> to make the law accessible, understandable, and tailored to your needs.
+            <h1> Welcome to <b>NYAAY SAHAYAAK</b>, your go-to destination for legal guidance and information! Our innovative legal chatbot is designed to simplify the complexities of laws and regulations, providing users with instant assistance and accurate information. Whether you're navigating business laws, seeking legal advice, or simply curious about your rights, our chatbot is here to guide you through the legal landscape. Empower yourself with easy access to legal knowledge, and let our chatbot be your virtual legal companion. Trust <b>NYAAY SAHAYAAK</b> to make the law accessible, understandable, and tailored to your needs.
             </h1>
         </p>
     );
